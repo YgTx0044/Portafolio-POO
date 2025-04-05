6e5b95494317c945d6a2950285c7ce7f21d1e4f8
YgTx0044 <24690555@tecvalles.mx>
2025-04-04 18:38:22 -0600

    Create bulbasaur.py
    Signed-off-by: YgTx0044 <24690555@tecvalles.mx>

---
+++ codigo 2/bulbasaur.py
@@ -0,0 +1,11 @@
+from hierba import TipoHierba
+from venenoso import TipoVenenoso
+
+class Bulbasaur(TipoHierba, TipoVenenoso):
+    def __init__(self, defensa=111, ataque=118):
+        TipoHierba.__init__(self, "Bulbasaur", defensa, ataque)
+        TipoVenenoso.__init__(self, "Bulbasaur", defensa, ataque)
+        self.tipo = "Hierba / Venenoso"
+
+    def __str__(self):
+        return f"{self.nombre} ({self.tipo}) 🌱 / ☠️"
