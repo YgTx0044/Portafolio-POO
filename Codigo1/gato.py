ddbe4e7ffa3c6812e35443a7823de3b7866f893f
YgTx0044 <24690555@tecvalles.mx>
2025-05-26 19:29:23 -0600

    Update gato.py
    Signed-off-by: YgTx0044 <24690555@tecvalles.mx>

--- Codigo1/gato.py
+++ Codigo1/gato.py
@@ -1,16 +1,25 @@
 from animal import Animal
 
-# Clase hereda de Animal
+
 class Gato(Animal):
-  def __init__(self, nombre, edad, vidas):
-    super().__init__(nombre, edad)
-    self.vidas = vidas
+    def __init__(self, nombre, edad, vidas):
+        super().__init__(nombre, edad)
+        self.vidas = vidas
 
-  def hacer_sonido(self):
-    print(f"{self.nombre} esta maullando")
-    print("Miau 🐈")
+    def hacer_sonido(self):
+        # super().hacer_sonido()
+        # print(f'{self.nombre} esta maullando')
+        print("Miau! 🐈")
 
-  def sobrevive(self):
-    if self.edad > 15 or self.vidas > 1:
-      self.vidas -= 1
-      return self.vidas
+    def sobrevive(self):
+        if self.edad > 15 or self.vidas > 0:
+            self.vidas -= 1
+            self.edad = 1
+
+        return self.vidas > 0
+
+    '''def __del__(self):
+        print(f'{self.nombre} se fue al cielo de los gatos')'''
+
+    def comer(self):
+        print(f'{self.nombre} esta comiendo 🐟')
